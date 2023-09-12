ce69f2c45644a825e9a775eb05dc4baae2fe54ce
Timo Beer <timo.beer@stud.hs-emden-leer.de>
2023-09-12 12:32:40 +0200

    Refactoring

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def readFile(fileName: str) -> Tuple[list, list, int]:
 
     Returns:
         Tuple[list, list, int]: A tuple containing the public key (list), 
-                                cipher text (list), and amount (int) extracted 
+                                cipher text (list), and relationsAmount (int) extracted 
                                 from the file.
     """
     if verbose:
@@ -35,37 +35,37 @@ def readFile(fileName: str) -> Tuple[list, list, int]:
     # Separating the text into its variables.
     publicKey = text.split('[')[1].split(']')[0].split(',')
     cipherText = text.split('[')[2].split(']')[0].split(',')
-    amount = int(text.split('relations:')[1])
+    relationsAmount = int(text.split('relations:')[1])
 
-    return publicKey, cipherText, amount
+    return publicKey, cipherText, relationsAmount
 
 
-def generatePlainText(amount: int) -> numpy.ndarray:
+def generatePlainText(relationsAmount: int) -> numpy.ndarray:
     """
     Generate a 2D numpy array of random plain texts.
 
     Args:
-        amount (int): The number of basic elements/special relations.
+        relationsAmount (int): The number of basic elements/special relations.
         verbose (bool): Whether to print verbose messages.
 
     Returns:
-        numpy.ndarray: A 2D numpy array of shape (2 * amount², amount) 
+        numpy.ndarray: A 2D numpy array of shape (2 * relationsAmount, relationsAmount) 
                        containing random plain texts represented by 
                        binary values.
     """
-    plainTextAmount = 2 * math.pow(amount, 2)
+    plainTextAmount = 2 * math.pow(relationsAmount, 2)
 
     if verbose:
         print(f'Generating {round(plainTextAmount)} plain texts')
 
-    plainTextMatrix = numpy.zeros(shape=(1, amount), 
+    plainTextMatrix = numpy.zeros(shape=(1, relationsAmount), 
                                   dtype=numpy.bool_)
 
-    # Generate random plain texts until 2 * amount² rows were generated.
+    # Generate random plain texts until 2 * relationsAmount² rows were generated.
     while plainTextMatrix.shape[0] < plainTextAmount:
         plainTextMatrix = numpy.vstack((plainTextMatrix, 
                                         numpy.random.choice(a=numpy.array([True, False]), 
-                                                            size=(1, amount))))
+                                                            size=(1, relationsAmount))))
 
     return plainTextMatrix
 
@@ -274,76 +274,78 @@ def getBaseVectors(matrix: numpy.ndarray, freeVariables: List[int]) -> List[nump
     return baseVectors
 
 
-def calculateRelationsMatrix(baseVectorsArray, amount, cipherText):
+def calculateRelationsMatrix(baseVectors: List[numpy.ndarray], relationsAmount: int, cipherTextMatrix: numpy.ndarray) -> numpy.ndarray:
+    """
+    Calculate a relations matrix based on base vectors and a cipher text matrix.
+
+    Args:
+        baseVectors (List[numpy.ndarray]): A list of numpy arrays representing base vectors.
+        relationsAmount (int): The number of relations to calculate.
+        cipherTextMatrix (numpy.ndarray): A 2D numpy array representing a cipher text matrix.
+
+    Returns:
+        numpy.ndarray: A 2D numpy array representing the relations matrix.
+    """
     if verbose:
         print('Calculating relations matrix')
     
-    relationsMatrix = numpy.zeros(shape=[0, amount], 
+    relationsMatrix = numpy.zeros(shape=[0, relationsAmount], 
                                   dtype=numpy.bool_)
 
-    for vector in baseVectorsArray:
-        relation = numpy.zeros([1, amount], dtype=numpy.intc)
-        # AND operation with the cipherText (size n) with n parts of the vector
-        for i in range(0, amount):
-            result = 0
-            for j in range(0, amount):
-                result += int(cipherText[j]) * vector[i * amount + j]
-            relation[0][i] = result % 2
+    # Logical AND every cipherText (of size 'relationsAmount') with 'relationsAmount' values of the vector
+    for vector in baseVectors:
+        relation = numpy.zeros(shape=[1, relationsAmount], 
+                               dtype=numpy.bool_)
+        for i in range(0, relationsAmount):
+            result = False
+            for j in range(0, relationsAmount):
+                result ^= numpy.logical_and(cipherTextMatrix[j][:], vector[i * relationsAmount + j])
+            relation[0][i] = result
         relationsMatrix = numpy.vstack((relationsMatrix, relation))
-
-    if verbose:
-        print(f'end:\trelation matrix calculated\n')
 
     return relationsMatrix
 
 
 def executePipeline(inputFile: str) -> None:
+    """
+    Execute a pipeline to solve the cryptographic problem using various matrix operations.
+
+    Args:
+        inputFile (str): The name of the input file containing the parameters.
+    """
     startingTime = time()
 
-    # Read parameters from the cryptoChallenge and generate matrix
-    publicKey, cipherText, amount = readFile(inputFile)
-    numpy.set_printoptions(threshold=sys.maxsize)
-    plainTextArray = generatePlainText(amount)
+    # Read the parameters from a formatted *.txt file and calculate a matrix to solve
+    publicKey, cipherText, relationsAmount = readFile(inputFile)
+    plainTextArray = generatePlainText(relationsAmount)
     cipherTextsArray = calculateCipherText(publicKey, plainTextArray)
     matrix = calculatingMatrix(plainTextArray, cipherTextsArray)
 
-    # Solve initial matrix
+    # Solve the initial matrix
     solvedMatrix = gaussianElimination(matrix)
-    freeVariablesArray = getFreeVariables(solvedMatrix)
-    reducedMatrix = reduceMatrix(solvedMatrix, freeVariablesArray)
-    baseVectorsArray = getBaseVectors(reducedMatrix, freeVariablesArray)
-    relationsMatrix = calculateRelationsMatrix(baseVectorsArray, amount, cipherText)
+    freeVariables = getFreeVariables(solvedMatrix)
+    reducedMatrix = reduceMatrix(solvedMatrix, freeVariables)
+    baseVectors = getBaseVectors(reducedMatrix, freeVariables)
+    relationsMatrix = calculateRelationsMatrix(baseVectors, relationsAmount, cipherText)
 
-    # Solve matrix consisting of the vectors
-    solvedRelationsMatrix = gaussianElimination(relationsMatrix, amount)
-    freeVariablesArraySolution = getFreeVariables(solvedRelationsMatrix)
-    reducedRelationsMatrix = reduceMatrix(solvedRelationsMatrix, freeVariablesArraySolution)
-    baseVectorsArraySolution = getBaseVectors(reducedRelationsMatrix, freeVariablesArraySolution)
+    # Solve the relations matrix
+    solvedRelationsMatrix = gaussianElimination(relationsMatrix, relationsAmount)
+    freeVariables = getFreeVariables(solvedRelationsMatrix)
+    reducedRelationsMatrix = reduceMatrix(solvedRelationsMatrix, freeVariables)
+    baseVectors = getBaseVectors(reducedRelationsMatrix, freeVariables)
 
-    # Verify result
-    resultcipherText = calculateCipherText(publicKey, numpy.array(baseVectorsArraySolution))
+    # Verify the result to insure that the calculation was right
+    cipherTextResult = calculateCipherText(publicKey, numpy.array(baseVectors))
     isCorrect = True
-    for i in range(0, resultcipherText.shape[1]):
-        if int(resultcipherText[0][i]) != int(cipherText[i]):
+    for i in range(0, cipherTextResult.shape[1]):
+        if int(cipherTextResult[0][i]) != int(cipherText[i]):
             isCorrect = False
-
-    if verbose:
-        print(f'generated PlainText:\n{plainTextArray}\n\n'
-              f'calculated cipherText:\n{cipherTextsArray}\n\n'
-              f'constructed matrix:\n{matrix}\n\n'
-              f'solved matrix:\n{solvedMatrix}\n\n'
-              f'reduced matrix:\n{reducedMatrix}\n\n'
-              f'free variables:\n{freeVariablesArray}\n\n'
-              f'base vectors:\n{baseVectorsArray}\n\n'
-              f'relation matrix:\n{relationsMatrix}\n\n'
-              f'free variables of the solution:\n{freeVariablesArraySolution}\n\n'
-              f'reduced relation matrix of the solution:\n{reducedRelationsMatrix}\n\n'
-              f'PlainText solution:\n{baseVectorsArraySolution}\n\n'
-              f'chitext of the solution:\n{resultcipherText}\n\n'
-              f'matching:\n{isCorrect}\n\n'
-              f'time:\n{time() - startingTime}\n')
+            
+    if isCorrect:
+        print(f'Plain text solution:\n{baseVectors}\n\n'
+              f'Solved in :\n{time() - startingTime} seconds')
     else:
-        print(baseVectorsArraySolution)
+        print('Decrypting failed')
 
 
 if __name__ == '__main__':
