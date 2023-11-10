b345c8750c8beb0de98b7dce3d85a6e61058331f
Timo Beer <timo.beer@stud.hs-emden-leer.de>
2023-11-10 22:20:19 +0100

    Refactoring of the structure and added docstrings

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import numpy as np
+from time import time
+from datetime import datetime
 from pipeline import executePipeline
 from argparse import ArgumentParser, Namespace
 
 
 def setupArgumentParser() -> Namespace:
+    """
+    Set up and configure the argument parser for the cryptographic decryption script.
+
+    Returns:
+        Namespace: A namespace containing the parsed command-line arguments.
+    """
     parser = ArgumentParser(description='Decrypt a ciphertext of an Matsumoto-Imai-Encryption based on the given public key.')
     parser.add_argument('filepath', 
                         type=str, 
@@ -15,5 +24,15 @@ def setupArgumentParser() -> Namespace:
 
 
 if __name__ == '__main__':
-    args = setupArgumentParser()
-    executePipeline(args)
+    startingTime = time()
+    
+    args = setupArgumentParser()    
+    baseVectors, isCorrect = executePipeline(args)
+        
+    currentTime = datetime.now().strftime("%H:%M:%S")  
+    if isCorrect:
+        print(f'[{currentTime}] Successful! Plain text solution: {np.array(baseVectors, dtype=np.uint8)}')
+        if args.verbose:
+              print(f'[{currentTime}] Solved in: {round((time() - startingTime), 2)} seconds')
+    else:
+        print(f'[{currentTime}] Decryption failed')
--- pipeline.py
+++ pipeline.py
@@ -1,12 +1,18 @@
 import numpy as np
 import functions as f
-from time import time
-from datetime import datetime
 from typing import List, Tuple
 from argparse import Namespace
 
 
 def calculateInitialMatrix(args: Namespace) -> Tuple[List[str], List[str], int, np.ndarray]:
+    """
+    Calculate the initial matrices and values required for the cryptographic problem.
+
+    Returns:
+        Tuple[List[str], List[str], int, np.ndarray]: A tuple containing the public key,
+                                                      cipher text, relationsAmount and the 
+                                                      initial matrix.
+    """
     publicKey, cipherText, relationsAmount = f.readFile(args)
     plainTextArray = f.generatePlainText(args, relationsAmount)
     cipherTextsArray = f.calculateCipherText(args, publicKey, plainTextArray)
@@ -16,6 +22,17 @@ def calculateInitialMatrix(args: Namespace) -> Tuple[List[str], List[str], int, 
 
 
 def solveInitialMatrix(args: Namespace, matrix: np.ndarray, relationsAmount: int, cipherText: List[str]) -> np.ndarray:
+    """
+    Solve the initial matrix and obtain the relations matrix.
+
+    Args:
+        matrix (np.ndarray): The initial matrix.
+        relationsAmount (int): The number of basic elements/special relations.
+        cipherText (List[str]): An array representing a cipher text.
+
+    Returns:
+        np.ndarray: A 2D numpy array representing the relations matrix.
+    """
     solvedMatrix = f.gaussianElimination(args, matrix)
     freeVariables = f.getFreeVariables(args, solvedMatrix)
     reducedMatrix = f.reduceMatrix(args, solvedMatrix, freeVariables)
@@ -26,6 +43,16 @@ def solveInitialMatrix(args: Namespace, matrix: np.ndarray, relationsAmount: int
 
 
 def solveRelationsMatrix(args: Namespace, relationsMatrix: np.ndarray) -> List[np.ndarray]:
+    """
+    Solve the relations matrix and obtain the base vectors (plain text of the intercepted
+    cipher text).
+
+    Args:
+        relationsMatrix (np.ndarray): A 2D numpy array representing the relations matrix.
+
+    Returns:
+        List[np.ndarray]: A list of a numpy array representing the base vector.
+    """
     solvedMatrix = f.gaussianElimination(args, relationsMatrix)
     freeVariables = f.getFreeVariables(args, solvedMatrix)
     reducedMatrix = f.reduceMatrix(args, solvedMatrix, freeVariables)
@@ -34,24 +61,20 @@ def solveRelationsMatrix(args: Namespace, relationsMatrix: np.ndarray) -> List[n
     return baseVectors
 
 
-def executePipeline(args: Namespace) -> None:
+def executePipeline(args: Namespace) -> Tuple[List[np.ndarray], bool]:
     """
-    Execute a pipeline to solve the cryptographic problem using various matrix operations.
-    """
-    startingTime = time()
+    Execute a pipeline to solve and verify the cryptographic problem.
 
+    Returns:
+        Tuple[List[np.ndarray], bool]: A tuple containing a list of a numpy array 
+                                       representing the base vector and a boolean 
+                                       indicating whether the verification of the 
+                                       result was successful.
+    """
     # Execute the pipeline
     publicKey, cipherText, relationsAmount, matrix = calculateInitialMatrix(args)
     relationsMatrix = solveInitialMatrix(args, matrix, relationsAmount, cipherText)
     baseVectors = solveRelationsMatrix(args, relationsMatrix)
 
     # Verify the result to insure that the calculation was right
-    isCorrect = f.verifyResult(args, publicKey, baseVectors, cipherText)
-    
-    currentTime = datetime.now().strftime("%H:%M:%S")  
-    if isCorrect:
-        print(f'[{currentTime}] Successful! Plain text solution: {np.array(baseVectors, dtype=np.uint8)}')
-        if args.verbose:
-              print(f'[{currentTime}] Solved in: {round((time() - startingTime), 2)} seconds')
-    else:
-        print(f'[{currentTime}] Decryption failed')
+    return baseVectors, f.verifyResult(args, publicKey, baseVectors, cipherText)
